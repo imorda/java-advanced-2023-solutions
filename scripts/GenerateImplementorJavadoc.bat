0b8e2c30cfe2d5b605cea74dca2bcc0fe0aa9f4f
imodre <btima@mail.ru>
2023-03-24 13:43:24 +0300

    * Javadoc fix

--- scripts/GenerateImplementorJavadoc.bat
+++ scripts/GenerateImplementorJavadoc.bat
@@ -1,5 +1,6 @@
 javadoc -private -author -version ^
     --module-path ..\..\java-advanced-2023\artifacts;..\..\java-advanced-2023\lib ^
     --source-path ..\..\java-advanced-2023\modules\info.kgeorgiy.java.advanced.implementor;..\java-solutions ^
+    -link https://docs.oracle.com/en/java/javase/19/docs/api/ ^
     -d ..\javadoc ^
     ..\java-solutions\info\kgeorgiy\ja\belousov\implementor\Implementor.java
