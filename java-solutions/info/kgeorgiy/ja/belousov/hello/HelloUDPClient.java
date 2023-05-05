dd9fefd231f01cff4438e4d94aa6c0d8314c5a11
imodre <btima@mail.ru>
2023-05-05 08:55:38 +0700

    + HW 9 implemented

---
+++ java-solutions/info/kgeorgiy/ja/belousov/hello/HelloUDPClient.java
@@ -0,0 +1,140 @@
+package info.kgeorgiy.ja.belousov.hello;
+
+import info.kgeorgiy.java.advanced.hello.HelloClient;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Implementation of a UDP client that concurrently sends requests in format
+ * "xxxyy_zz" where xxx - a given string, yy - number of a sender thread, zz - number of a request in a thread
+ */
+public class HelloUDPClient implements HelloClient {
+    private static final int SOCKET_READ_TIMEOUT_MILLIS = 200;
+
+    /**
+     * Main function used as entrypoint when launched as a standalone application
+     *
+     * @param args Required:
+     *             - Hostname (or ip address) of a server
+     *             - Server port number
+     *             - Requests prefix (xxx)
+     *             - Number of requests threads
+     *             - Number of requests inside each thread
+     */
+    public static void main(String[] args) {
+        if (args == null || args.length != 5) {
+            System.err.println("Usage: java HelloUDPClient <hostname> <port> <request> <threads> <requests>");
+            return;
+        }
+
+        String hostname = args[0];
+        int port;
+        String request = args[2];
+        int threads;
+        int requests;
+        try {
+            Objects.requireNonNull(hostname);
+            Objects.requireNonNull(request);
+
+            port = Integer.parseInt(args[1]);
+            threads = Integer.parseInt(args[3]);
+            requests = Integer.parseInt(args[4]);
+        } catch (NumberFormatException | NullPointerException e) {
+            System.err.println("Incorrect args format!");
+            return;
+        }
+
+        HelloUDPClient instance = new HelloUDPClient();
+        try {
+            instance.run(hostname, port, request, threads, requests);
+        } catch (IllegalArgumentException e) {
+            System.err.printf("Incorrect port number: %s%n", e.getMessage());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param host     server host
+     * @param port     server port
+     * @param prefix   request prefix
+     * @param threads  number of request threads
+     * @param requests number of requests per thread.
+     * @throws IllegalArgumentException when port is out of range
+     */
+    @Override
+    public void run(String host, int port, String prefix, int threads, int requests) {
+        SocketAddress address = new InetSocketAddress(host, port);
+
+        List<Thread> threadInstances = new ArrayList<>(threads);
+        for (int thread = 0; thread < threads; thread++) {
+            final int threadFinal = thread;
+            threadInstances.add(new Thread(() -> {
+                try (DatagramSocket socket = new DatagramSocket()) {
+                    socket.setSoTimeout(SOCKET_READ_TIMEOUT_MILLIS);
+
+                    for (int request = 0; request < requests; ++request) {
+                        String requestText = String.format("%s%d_%d", prefix, threadFinal, request);
+                        DatagramPacket packet = new DatagramPacket(requestText.getBytes(), requestText.length(), address);
+
+                        while (!socket.isClosed() && !Thread.currentThread().isInterrupted()) {
+                            try {
+                                socket.send(packet);
+                            } catch (IOException | IllegalArgumentException e) {
+                                System.err.format("Error sending packet: %s%n", e.getMessage());
+                            }
+
+                            byte[] buffer = new byte[requestText.length() + HelloUDPServer.RESPONSE_PREFIX.length()];
+                            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+
+                            try {
+                                socket.receive(response);
+                            } catch (SocketTimeoutException ignored) {
+                                continue;
+                            } catch (IOException e) {
+                                System.err.format("Error receiving packet: %s%n", e.getMessage());
+                            }
+
+                            if (response.getLength() > 0) {
+                                String expectedResponse = HelloUDPServer.RESPONSE_PREFIX + requestText;
+                                String responseText = new String(response.getData(), response.getOffset(), response.getLength());
+
+                                if (!expectedResponse.equals(responseText)) {
+                                    printReport(System.err, requestText, responseText);
+                                    continue;
+                                }
+                                printReport(System.out, requestText, responseText);
+                                break;
+                            }
+                        }
+                        if (Thread.interrupted()) {
+                            break;
+                        }
+                    }
+                } catch (SocketException e) {
+                    System.err.printf("Can't open socket in thread %d: %s", threadFinal, e);
+                }
+            }));
+        }
+
+        threadInstances.forEach(Thread::start);
+
+        for (int i = 0; i < threads; i++) {
+            try {
+                threadInstances.get(i).join();
+            } catch (InterruptedException ignored) { // Main thread was interrupted
+                i--; // Try again from the same thread
+            }
+        }
+    }
+
+    private void printReport(final PrintStream stream, final String request, final String response) {
+        stream.printf("> \"%s\"%n  < \"%s\"%n", request, response);
+    }
+}
+
---
+++ java-solutions/info/kgeorgiy/ja/belousov/hello/HelloUDPServer.java
@@ -0,0 +1,137 @@
+package info.kgeorgiy.ja.belousov.hello;
+
+import info.kgeorgiy.java.advanced.hello.HelloServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Implementation of a UDP server that echoes any request adding a prefix "Hello, " to it
+ */
+public class HelloUDPServer implements HelloServer {
+    static final String RESPONSE_PREFIX = "Hello, ";
+
+    private DatagramSocket socket = null;
+    private Thread serverThread = null;
+    private ExecutorService workers = null;
+
+    /**
+     * Main function used as entrypoint when launched as a standalone application
+     *
+     * @param args Required:
+     *             - Server port number
+     *             - Number of worker threads
+     */
+    public static void main(String[] args) {
+        if (args == null || args.length != 2) {
+            System.err.println("Usage: java HelloUDPServer <port> <threads>");
+            return;
+        }
+
+        int port;
+        int threads;
+        try {
+            port = Integer.parseInt(args[0]);
+            threads = Integer.parseInt(args[1]);
+        } catch (NumberFormatException | NullPointerException e) {
+            System.err.println("Incorrect args format!");
+            return;
+        }
+
+        HelloUDPServer instance = new HelloUDPServer();
+        try {
+            instance.start(port, threads);
+        } catch (IllegalArgumentException e) {
+            System.err.printf("Incorrect port number: %s%n", e.getMessage());
+        } catch (IllegalStateException e) {
+            System.err.printf("Cannot bind socket: %s%n", e.getMessage());
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(instance::close));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param port    server port.
+     * @param threads number of working threads.
+     * @throws IllegalStateException    if the socket can't be bound
+     * @throws IllegalArgumentException if port is out of range
+     */
+    @Override
+    public void start(int port, int threads) {
+        close();
+
+        Semaphore availableResource = new Semaphore(threads);
+        workers = Executors.newFixedThreadPool(threads);
+        try {
+            socket = new DatagramSocket(port);
+            serverThread = new Thread(() -> {
+                try {
+                    while (!socket.isClosed() && !Thread.interrupted()) {
+                        try {
+                            DatagramPacket packet = new DatagramPacket(
+                                    new byte[socket.getReceiveBufferSize()], socket.getReceiveBufferSize());
+
+                            socket.receive(packet);
+
+                            availableResource.acquire();
+                            try {
+                                workers.submit(() -> {
+                                    try {
+                                        String packetText = new String(packet.getData(), packet.getOffset(), packet.getLength());
+                                        String responseText = RESPONSE_PREFIX + packetText;
+
+                                        DatagramPacket response = new DatagramPacket(responseText.getBytes(),
+                                                responseText.length(), packet.getSocketAddress());
+
+                                        try {
+                                            socket.send(response);
+                                        } catch (IOException | IllegalArgumentException e) {
+                                            System.err.format("Error sending packet: %s%n", e.getMessage());
+                                        }
+                                    } finally {
+                                        availableResource.release();
+                                    }
+                                });
+                            } catch (RejectedExecutionException ignored) {
+                                availableResource.release();
+                            }
+                        } catch (SocketException e) {
+                            if (!socket.isClosed()) {
+                                System.err.printf("Server UDP error: %s%n", e.getMessage());
+                            }
+                        } catch (IOException e) {
+                            System.err.printf("IO error in server socket: %s%n", e.getMessage());
+                        }
+                    }
+                } catch (InterruptedException ignored) {
+                } finally {
+                    Thread.currentThread().interrupt();
+                }
+            });
+            serverThread.start();
+        } catch (SocketException e) {
+            throw new IllegalStateException("Error opening socket", e);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (socket != null) {
+            socket.close();
+        }
+        if (serverThread != null) {
+            serverThread.interrupt();
+        }
+        if (workers != null) {
+            workers.shutdownNow();
+        }
+    }
+}
